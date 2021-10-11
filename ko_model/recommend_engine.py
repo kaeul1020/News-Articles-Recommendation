68ce055f57eee8f04014749c5fda15061580b9f5
OhJiwoo <jiu3159@naver.com>
2021-10-11 21:03:50 +0900

    Content based 방식과 Hybrid 방식 코드를 합침
    content based 방식에 대해선 동작이 되고 있고,
    hybrid 방식은 textblob에서 문제가 있어,
    이후 가을님이 제안한 좋아요/싫어요 방식으로 수정필요
    또한 content based 방식과 hybrid 방식 전환을 위한 디비 연결 필요

--- ko_model/recommend_engine.py
+++ ko_model/recommend_engine.py
@@ -11,6 +11,62 @@ from gensim.models import Phrases
 
 import koreanToken
 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+class Content:
+    def __init__(self, df):
+        self.df = df
+        self.cosine_sim, self.indices = self.base()
+        self.news = input("보고싶은 뉴스를 입력하세요! ")  # 이 입력은 데이터베이스로부터 현재 읽은 뉴스 제목을 받아옴.
+        self.rec()
+
+    def base(self):
+        # TF-IDF Vectorizer Object 정의. stopword 제거(쓸모 없지만 자주 사용되는 단어는 제거)
+        tfidf = TfidfVectorizer()
+        #  NaN을 empty string으로 변환
+        self.df['content'] = self.df['content'].fillna('')
+        #  content 항목에 대한 TF-IDF matrix 생성
+        tfidf_matrix = tfidf.fit_transform(self.df['content'])
+
+        # (TFIDF vector 이용할 것이므로) linear_kernel을 대신 이용하여 dot product 계산
+        # cosine similarity matrix와 같은 결과
+        cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+        # index와 news title에 대해 reverse mapping 후 중복된 title 제거
+        indices = pd.Series(self.df.index, index=self.df['title']).drop_duplicates()
+
+        return cosine_sim, indices
+
+    def content_recommender(self):
+        # title에 해당하는 news index
+        idx = self.indices[self.news]
+
+        # 해당 news를 기준으로  pairwsie similarity score 계산 후  convert into a list of tuples
+        sim_scores = list(enumerate(self.cosine_sim[idx]))
+
+        # cosine similarity scores 기준으로 정렬
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # 10 most similar news의 score. 단, first news 제외(왜냐하면 similarity score  with itself)
+        sim_scores = sim_scores[1:5]  # cosine 유사도가 높은 1~10까지의 기사
+
+        # news indices 생성
+        news_indices = [i[0] for i in sim_scores]
+
+        # top 10 most similar news
+        return self.df['title'].iloc[news_indices], news_indices
+
+    # 추천항목 도출
+    def rec(self):
+        title, index = self.content_recommender()
+        i = 1
+        for a in index:
+            print(str(i) + ') ' + str(title[a] + '\n'))  # 글자 삽입
+            i += 1
+
+
 class Process:
     def __init__(self):
         self.to_corpus = []
